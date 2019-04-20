decc6fba46e291ed8c1babe5b8f98cd8fc25fd61
leonid <lshishkin150586@gmail.com>
2019-04-20 12:10:22 +0300

    second commit

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 const path = require('path');
 const favicon = require('serve-favicon');
 const compress = require('compression');
-const helmet = require('helmet');
 const cors = require('cors');
-const logger = require('./logger');
+const helmet = require('helmet');
+const logger = require('winston');
 
 const feathers = require('@feathersjs/feathers');
 const configuration = require('@feathersjs/configuration');
@@ -18,15 +18,13 @@ const channels = require('./channels');
 
 const authentication = require('./authentication');
 
-const mongoose = require('./mongoose');
-
 const app = express(feathers());
 
 // Load app configuration
 app.configure(configuration());
-// Enable security, CORS, compression, favicon and body parsing
-app.use(helmet());
+// Enable CORS, security, compression, favicon and body parsing
 app.use(cors());
+app.use(helmet());
 app.use(compress());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -37,8 +35,6 @@ app.use('/', express.static(app.get('public')));
 // Set up Plugins and providers
 app.configure(express.rest());
 app.configure(socketio());
-
-app.configure(mongoose);
 
 // Configure other middleware (see `middleware/index.js`)
 app.configure(middleware);
